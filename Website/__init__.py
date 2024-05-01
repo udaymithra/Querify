cfd2335f58dc5fdad2dc1f6e0c53e4c6ea4df11f
Udaymithra Kalla <udaymithrakalla@Udaymithras-MacBook-Pro.local>
2024-05-01 14:49:53 +0800

    Initializing the App

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,19 +1,8 @@
+#Initializing the app
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-db = SQLAlchemy()
-DB_NAME='database.db'
-# Improved configuration (consider using environment variables)
-app_config = {
-    'SECRET_KEY': 'heqpoc gadjhhjhj',  # Replace with a strong secret key
-    'SQLALCHEMY_DATABASE_URI': f'sqlite:///{DB_NAME}',  # Replace DB_NAME with your filename
-    'SQLALCHEMY_TRACK_MODIFICATIONS': False
-}
-
 def create_app():
     app = Flask(__name__)
-    app.config.update(app_config)
-    
     
     return app
