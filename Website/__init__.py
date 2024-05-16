14d7f0796ab4a57fd4cfd67dec789a71a7bce1ea
dharaninagala <dharaninagala13@gmail.com>
2024-05-17 01:32:42 +0800

    Implemented Likes-Dislikes for Post-Comment feature

--- Website/__init__.py
+++ Website/__init__.py
@@ -34,7 +34,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Post
-    from .views import likes_count_by_id,update_count_by_id,delete_count_by_id
+    from .views import likes_count_by_id,update_count_by_id,delete_count_by_id,like_count_of_comment_by_id,update_comment_count_by_id,delete_comment_count_by_id
     # Create tables outside the context for reliable execution
     with app.app_context():
         db.create_all()
@@ -46,6 +46,8 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+    
+    # Post Like and Dislike Functionality
     @socketio.on('like_update_count')
     def like_update_count(data):
         post_id = data.get('post_id')  # Assuming data contains 'post_id' key
@@ -62,6 +64,28 @@ def create_app():
             socketio.emit('dislike_count_updated', {'post_id': post_id, 'count': new_count})
         else:
             socketio.emit('dislike_count_updated', {'post_id': post_id, 'count': 0})
+    #Comment Like and Dislike Functionality
+    @socketio.on('like_update_comment_count')
+    def like_update_comment_count(data):
+        user_id = data.get('user_id') 
+        comment_id=data.get('comment_id')
+        print("CommentId",comment_id)
+        new_count = like_count_of_comment_by_id(user_id,comment_id) + 1  # Example increment
+        update_comment_count_by_id(user_id,comment_id)
+        socketio.emit('like_update_comment_count', {'comment_id':comment_id , 'count': new_count})
+
+    @socketio.on('dislike_update_comment_count')
+    def dislike_update_comment_count(data):
+        user_id = data.get('user_id') 
+        comment_id=data.get('comment_id')
+        print("In Dislike Comment Func")
+        deleted=delete_comment_count_by_id(user_id,comment_id)
+        if deleted:
+            new_count=like_count_of_comment_by_id(user_id,comment_id)
+            socketio.emit('dislike_update_comment_count', {'comment_id': comment_id, 'count': new_count})
+        else:
+            socketio.emit('dislike_update_comment_count', {'comment_id': comment_id, 'count': 0})
+
 
 
     return app
