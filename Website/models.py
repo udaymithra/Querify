2daf75b2f99419ba09541f209b29ed2c34e5ea1f
Sem3group <shanmugapriyasankarraj@gmail.com>
2024-05-18 16:35:01 +0800

    Database Refactored

--- Website/models.py
+++ Website/models.py
@@ -3,8 +3,13 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func  #for fetching the current timestamp for posting 
 from flask_login import current_user
 
-#creating a Post Schema model for database
+#This Function is for Fetching the current user
+def get_user_id():
+    return current_user.id if current_user.is_authenticated else None
 
+
+
+#creating a Post Schema model for storing post information
 class Post(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     userId=db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -12,14 +17,7 @@ class Post(db.Model):
     content=db.Column(db.Text)
     date=db.Column(db.DateTime(timezone='Asia/Kolkata'), default=func.date(func.now()))
     
-
-
-
-
-
-
-#creating a User Schema model for database 
-
+#creating a User Schema model for storing users information 
 class User(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     firstName=db.Column(db.String(150))
@@ -28,25 +26,20 @@ class User(db.Model, UserMixin):
     password=db.Column(db.String(150))
     posts=db.relationship('Post')
 
-
-
-
-def get_user_id():
-    return current_user.id if current_user.is_authenticated else None
-
+#Creating a Likes Schema Model for storing like interactions on post by Users
 class Likes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     postId = db.Column(db.Integer, db.ForeignKey('post.id'))
     userId = db.Column(db.Integer, default=get_user_id)
  
-
+#Creating a Comments Schema Model for Storing Comments by Users
 class Comments(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer)
     postId=db.Column(db.Integer, db.ForeignKey('post.id'))
     content=db.Column(db.Text)
     date=db.Column(db.DateTime(timezone='Asia/Kolkata'), default=func.date(func.now()))
-
+#Creating a Reply Schema Model for storing Replies by Users
 class Reply(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     commentId=db.Column(db.Integer, db.ForeignKey('comments.id'))
@@ -56,7 +49,7 @@ class Reply(db.Model):
     repliedTo=db.Column(db.Text)
     postId=db.Column(db.Integer)
     userId=db.Column(db.Integer)
-
+#Creating a Comments Like Schema for storing like interactions on Comments by Users
 class CommentsLike(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     commentId=db.Column(db.Integer, db.ForeignKey('comments.id'))
