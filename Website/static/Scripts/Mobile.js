fa2787ac9c1c77fe8c0c5572497b2af984a44e19
gnaneshwarbana <gnaneshwarbana@gmail.com>
2024-05-18 16:54:50 +0800

    Fixed Minor UI Bugs

--- Website/static/Scripts/Mobile.js
+++ Website/static/Scripts/Mobile.js
@@ -1,10 +1,8 @@
-//Search click action in Mobile Navigation bar
 document.querySelector("#searchMob").addEventListener("click", () => {
   document.querySelector(".mobNav").style.display = "none";
   document.querySelector(".mobSearchBar").style.display = "flex";
   document.querySelector(".mobSearchBar").style.opacity = "1";
 });
-//Back click action in search bar(mobile navigation)
 document.querySelector("#backMob").addEventListener("click", () => {
   document.querySelector(".mobNav").style.display = "flex";
   document.querySelector(".mobSearchBar").style.opacity = "0";
